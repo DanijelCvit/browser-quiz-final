001f57888eb144225e51e9621282bb629e052fd0
Danijel C <danijelc+hyf@gmail.com>
2021-11-13 22:07:53 +0100

    Fixes spelling issue

--- src/pages/popup.js
+++
@@ -1,6 +0,0 @@
-import { createPopup } from '../components/popup.html.js';
-
-export const initializePopup = () => {
-  const popupTemplate = createPopup();
-  document.body.innerHTML += popupTemplate;
-};
--- src/views/start.html.js
+++ src/views/start.html.js
@@ -4,7 +4,7 @@ export const createStart = () => {
   return String.raw`
     
     <div class=${START_PAGE}>
-    <p>WELCOME IN </p>
+    <p>WELCOME TO </p>
     <p>A-TEAM QUIZ</p>
     <a class="start-button btn btn-block btn-dark btn-block" id='startButton' href='?page=quiz&question=0'>Start Quiz</a>
     </div>
